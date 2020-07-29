8d063e3716e360f24ec76677243791cb6f1f93c7
Russell Carey <russellwcarey@gmail.com>
2020-07-28 22:39:18 -0600

    quick update with new googlejs folder

---
+++ google.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'Google advanced search: Elon Musk'(browser) {
+    const mainQuery = 'Elon Musk';
+    const mainQueryInputSelector = 'name="as_q"';
+    browser
+      .url('https://www.google.com/advanced_search')
+
+  }
+}
