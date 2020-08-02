043339b8f5b8e66a8364b0f918474cb039b650be
Russell Carey <russellwcarey@gmail.com>
2020-08-02 16:26:29 -0600

    quick update to googlejs file

--- google.js
+++ google.js
@@ -1,9 +1,10 @@
 module.exports = {
   'Google advanced search: Elon Musk'(browser) {
     const mainQuery = 'Elon Musk';
+
     const mainQueryInputSelector = 'name="as_q"';
     browser
       .url('https://www.google.com/advanced_search')
-
+      .setValue()
   }
 }
